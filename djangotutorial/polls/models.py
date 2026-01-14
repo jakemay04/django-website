4881a392a97aa032bb4a6c4bacfefb7515f8462d
jakemay04 <jmay90@byu.edu>
2026-01-14 13:06:09 -0700

    Adding str methods to class to allow db viewing of data in cmd

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -7,8 +7,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Date Published")
 
+    def __str__(self):
+        return self.question_text #method to make question visable as string in command line
+
+
 #model representing voting choice logic, creating foreign key to link question to choice. also includes voting/text logic
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE) #Names like question, choice_text and votes will be used as columns names in DB
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text #Same as question class, makes question visable in cmd
